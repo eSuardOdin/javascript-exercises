c75b11fb327f35f0fac95fb78b130abefd9a6e99
eSuardOdin <esuardodin@gmail.com>
2023-01-07 12:32:51 +0100

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...x) {
+    const arr = arguments[String(0)];
+    let newArr = arr;
+    for(let i = 1; i < arguments.length; i++) {
+        newArr = newArr.filter(el => el !== arguments[i]);
+    }
+    return newArr;
 };
+// console.log(removeFromArray([1,2,3,4,5,'roger','lol',(52+2)], 2,'roger','loooool', 54));
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
