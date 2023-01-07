ad526b40eabaa7cf2e581e5ea8a4370d8e19e283
eSuardOdin <esuardodin@gmail.com>
2023-01-07 12:00:06 +0100

    Done until reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let res = '';
+    for (let i = str.length-1; i >= 0; i--) {
+        res += str.charAt(i);
+    }
+    return res; 
 };
 
+console.log(reverseString('hello'));
 // Do not edit below this line
 module.exports = reverseString;
