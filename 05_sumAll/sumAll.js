003ee5113bd6539f999bacbb667ec213026fe649
eSuardOdin <esuardodin@gmail.com>
2023-01-07 12:49:48 +0100

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(nb1, nb2) {
+    if((typeof nb1) !== "number" || (typeof nb2) !== "number" || nb1 < 0 || nb2 < 0) return 'ERROR';
+    let res = 0, largest = 0, lowest = 0;
+    if (nb1 < nb2) {
+        largest = nb2;
+        lowest = nb1; 
+    } else {
+        largest = nb1;
+        lowest = nb2;
+    }
+    while(lowest <= largest) {
+        res += lowest;
+        lowest++;
+    }
+    return res; 
 };
-
 // Do not edit below this line
 module.exports = sumAll;
