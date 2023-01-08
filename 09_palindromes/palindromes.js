a925cc75bb030f4ef026fd2feae902f627c9ed49
eSuardOdin <esuardodin@gmail.com>
2023-01-08 11:48:03 +0100

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newStr = '';
+    let reverseStr = '';
+    for(let i = 0; i < str.length; i++) {
+        if(/^[A-Za-z]*$/.test(str.charAt(i))) {
+            newStr += str.charAt(i).toLowerCase();
+        }
+    } 
+    for(let j = newStr.length-1; j >= 0; j--) {
+        reverseStr += newStr.charAt(j);
+    }
+    return (newStr === reverseStr); 
 };
 
+
+// console.log(palindromes('kayak kayak'));
+// console.log(palindromes('abcba'));
+// console.log(palindromes('abcbazzd  zdzd'));
 // Do not edit below this line
 module.exports = palindromes;
