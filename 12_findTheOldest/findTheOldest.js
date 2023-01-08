1cf8b657d261d86bb57d47f43fd41b09b49a8a05
eSuardOdin <esuardodin@gmail.com>
2023-01-08 13:25:28 +0100

    12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const year = new Date().getFullYear();
+    const getAge = (person) => {
+        return (person.hasOwnProperty('yearOfDeath') ? person.yearOfDeath - person.yearOfBirth : year - person.yearOfBirth);
+    }
+    let oldest = people.reduce((firstPers, x) => {
+        firstPers = getAge(firstPers) < getAge(x) ? x : firstPers;
+        return firstPers;
+    });
+    return oldest;
 };
 
 // Do not edit below this line
