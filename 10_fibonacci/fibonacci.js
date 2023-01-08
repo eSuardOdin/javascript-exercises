ad45c554a1e091fb0042c658b9da9019033d41f1
eSuardOdin <esuardodin@gmail.com>
2023-01-08 12:29:48 +0100

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(baseNb) {
+    const nb = Number(baseNb);
+    if (nb < 0) return 'OOPS';
+    let a = 0;
+    let b = 1;
+    let c = 0;
+    for(let i = 1; i <= nb; i++) {
+        c = a;
+        a = b;
+        b += c;
+    }
+    return a;
 };
+
+// console.log(fibonacci(6));
+// console.log(fibonacci('6'));
+
 
 // Do not edit below this line
 module.exports = fibonacci;
